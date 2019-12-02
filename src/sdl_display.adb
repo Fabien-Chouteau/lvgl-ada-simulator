b828e441c070ce7c5ce52aaadf06e2092ac4fea9
Fabien Chouteau <fabien.chouteau@gmail.com>
2019-12-02 20:46:41 +0100

    Switch to SDL2 with Alire and move example to a dedicated repo

--- src/sdl_display.adb
+++ src/sdl_display.adb
@@ -1,6 +1,25 @@
+with Ada.Unchecked_Conversion;
+with SDL.Video.Pixels;
+with System;
 with Interfaces.C;    use Interfaces.C;
-with SDL_SDL_h;       use SDL_SDL_h;
-with SDL_SDL_video_h; use SDL_SDL_video_h;
+
+with SDL;
+with SDL.Video.Windows;
+with SDL.Video.Windows.Makers;
+with SDL.Video.Surfaces;
+with SDL.Video.Pixel_Formats;
+with SDL.Video.Palettes; use SDL.Video.Palettes;
+with SDL.Video.Pixel_Formats; use SDL.Video.Pixel_Formats;
+with SDL.Video.Textures; use SDL.Video.Textures;
+with SDL.Video.Textures.Makers;
+with SDL.Video.Renderers;
+with SDL.Video.Renderers.Makers;
+
+with SDL.Events;           use SDL.Events;
+with SDL.Events.Events;    use SDL.Events.Events;
+with SDL.Events.Keyboards; use SDL.Events.Keyboards;
+with SDL.Events.Mice;      use SDL.Events.Mice;
+with SDL.Inputs.Mice;      use SDL.Inputs.Mice;
 
 with LV; use LV;
 with LV.Color;
@@ -12,16 +31,27 @@ with LV.Objx;
 with LV.Objx.Img;
 with Ada.Text_IO;
 
-with SDL_SDL_mouse_h;
-with SDL_SDL_stdinc_h;
-with SDL_SDL_events_h; use SDL_SDL_events_h;
-with SDL_SDL_keysym_h; use SDL_SDL_keysym_h;
-
 with GNAT.OS_Lib;
 
 package body SDL_Display is
 
-   Display : access SDL_Surface;
+   W          : SDL.Video.Windows.Window;
+   Renderer   : SDL.Video.Renderers.Renderer;
+   Texture    : SDL.Video.Textures.Texture;
+   SDL_Pixels : System.Address;
+
+   type Texture_1D_Array is array (Int32_T range <>)
+     of aliased SDL_Pixel;
+
+   procedure Lock is new SDL.Video.Textures.Lock
+     (Pixel_Pointer_Type => System.Address);
+
+   function To_Address is
+     new Ada.Unchecked_Conversion
+       (Source => SDL.Video.Pixels.ARGB_8888_Access.Pointer,
+        Target => System.Address);
+
+   procedure Update;
 
    LV_Disp_Drv : aliased Disp_Drv_T;
    LV_Disp : Disp_T;
@@ -66,20 +96,27 @@ package body SDL_Display is
       pragma Import (C, Mouse_Cursor_Icon, "mouse_cursor_icon");
    begin
 
-      if SDL_Init (SDL_INIT_VIDEO) < 0 then
+      if not SDL.Initialise (Flags => SDL.Enable_Screen) then
          raise Program_Error with "SDL Video init failed";
-         return;
       end if;
 
-      Display := SDL_SetVideoMode (int (Width),
-                                   int (Height),
-                                   SDL_Pixel'Size,
-                                   SDL_SWSURFACE);
+      SDL.Video.Windows.Makers.Create
+        (W, "LVGL-Ada Example",
+         0,
+         0,
+         Integer (Width),
+         Integer (Height),
+         Flags    => SDL.Video.Windows.Resizable);
 
-      if Display = null then
-         raise Program_Error with "Cannot create SDL display";
-      end if;
+      SDL.Video.Renderers.Makers.Create (Renderer, W);
 
+      SDL.Video.Textures.Makers.Create
+        (Tex      => Texture,
+         Renderer => Renderer,
+         Format   => SDL.Video.Pixel_Formats.Pixel_Format_RGB_565,
+         Kind     => SDL.Video.Textures.Streaming,
+         Size     => (Integer (Width),
+                      Integer (Height)));
       LV.HAL.Disp.Init_Drv (LV_Disp_Drv'Access);
 
       LV_Disp_Drv.disp_flush := disp_flush'Access;
@@ -100,33 +137,51 @@ package body SDL_Display is
       LV.Indev.set_cursor (LV_Indev, Cursor_Obj);
    end Initialize;
 
+   ------------
+   -- Update --
+   ------------
+
+   procedure Update is
+      Width  : constant Natural := Texture.Get_Size.Width;
+      Height : constant Natural := Texture.Get_Size.Height;
+   begin
+
+      Renderer.Clear;
+      Renderer.Copy (Texture, To => (0,
+                                     0,
+                                     int (Width),
+                                     int (Height)));
+      Renderer.Present;
+   end Update;
+
    ----------
    -- Read --
    ----------
 
    function Read (Data : access indev_data_t) return u_Bool is
-      X, Y : aliased Interfaces.C.int;
-      State : SDL_SDL_stdinc_h.Uint8;
-      Evt : aliased SDL_Event;
-   begin
-      while SDL_PollEvent (Evt'Access) /= 0 loop
+      Event   : SDL.Events.Events.Events;
 
-         if Evt.c_type = SDL_KEYDOWN or else Evt.c_type = SDL_KEYUP then
-            case Evt.key.keysym.sym is
-            when SDLK_ESCAPE =>
-               if Evt.c_type = SDL_KEYDOWN then
-                  GNAT.OS_Lib.OS_Exit (0);
-               end if;
+      Mouse_Button_Masks : SDL.Events.Mice.Button_Masks;
+      X, Y : SDL.Events.Mice.Movement_Values;
+   begin
+      while SDL.Events.Events.Poll (Event) loop
+         if Event.Common.Event_Type in Key_Down | Key_Up then
+
+            case Event.Keyboard.Key_Sym.Scan_Code is
+            when Scan_Code_Escape =>
+               GNAT.OS_Lib.OS_Exit (0);
             when others =>
                null;
             end case;
          end if;
       end loop;
 
-      State := SDL_SDL_mouse_h.SDL_GetMouseState (X'Access, Y'Access);
+
+
+      Mouse_Button_Masks := SDL.Inputs.Mice.Get_State (X, Y);
 
       Data.Union.point := (LV.Int16_T (X), LV.Int16_T (Y));
-      if (State and SDL_SDL_mouse_h.SDL_BUTTON_LEFT) /= 0 then
+      if (Mouse_Button_Masks and Left_Mask) /= 0 then
          Data.state := LV.HAL.Indev.STATE_PR;
       else
          Data.state := LV.HAL.Indev.STATE_REL;
@@ -148,11 +203,9 @@ package body SDL_Display is
       y2    : int32_t;
       Color : access constant Color_Array)
    is
-      Buffer : array (0 .. Int32_T (Display.w * Display.h - 1))
-        of SDL_Pixel
-          with Address => Display.pixels;
 
-      Index : Natural := Color'First;
+      Width  : constant Natural := Texture.Get_Size.Width;
+      Height : constant Natural := Texture.Get_Size.Height;
    begin
       if X2 < 0
         or else
@@ -166,20 +219,28 @@ package body SDL_Display is
          return;
       end if;
 
-      for Y in Y1 .. Y2 loop
-         for X in X1 .. X2 loop
-            Buffer (Y * Int32_T (Width) + X) := LV.Color.Color_To16 (Color (Index));
-            Index := Index + 1;
-         end loop;
-      end loop;
+      Lock (Texture, SDL_Pixels);
 
-      SDL_UpdateRect (Display,
-                      int (X1),
-                      int (Y1),
-                      unsigned (X2 - X1 + 1),
-                      unsigned (Y2 - Y1 + 1));
+      declare
+         Actual_Pixels : Texture_1D_Array (0 .. Int32_T (Width * Height - 1))
+           with
+             Address => SDL_Pixels;
+
+         Index : Natural := Color'First;
+      begin
+
+         for Y in Y1 .. Y2 loop
+            for X in X1 .. X2 loop
+               Actual_Pixels (Y * Int32_T (Width) + X)
+                 := LV.Color.Color_To16 (Color (Index));
+               Index := Index + 1;
+            end loop;
+         end loop;
+      end;
 
       LV.VDB.Flush_Ready;
+      Texture.Unlock;
+      Update;
    end Disp_Flush;
 
    ---------------
@@ -193,11 +254,10 @@ package body SDL_Display is
       y2    : int32_t;
       Color : LV.Color.Color_T)
    is
-      Buffer : array (0 .. Int32_T (Display.w * Display.h - 1))
-        of SDL_Pixel
-          with Address => Display.pixels;
 
-      C : constant Uint16_T := LV.Color.Color_To16 (Color);
+      Width  : constant Natural := Texture.Get_Size.Width;
+      Height : constant Natural := Texture.Get_Size.Height;
+      C      : constant Uint16_T := LV.Color.Color_To16 (Color);
    begin
       if X2 < 0
         or else
@@ -211,17 +271,24 @@ package body SDL_Display is
          return;
       end if;
 
-      for Y in Y1 .. Y2 loop
-         for X in X1 .. X2 loop
-            Buffer (Y * Int32_T (Width) + X) := C;
-         end loop;
-      end loop;
+      Lock (Texture, SDL_Pixels);
 
-      SDL_UpdateRect (Display,
-                      int (X1),
-                      int (Y1),
-                      unsigned (X2 - X1 + 1),
-                      unsigned (Y2 - Y1 + 1));
+      declare
+         Actual_Pixels : Texture_1D_Array (0 .. Int32_T (Width * Height - 1))
+           with
+             Address => SDL_Pixels;
+      begin
+
+         for Y in Y1 .. Y2 loop
+            for X in X1 .. X2 loop
+               Actual_Pixels (Y * Int32_T (Width) + X) := C;
+            end loop;
+         end loop;
+      end;
+
+      LV.VDB.Flush_Ready;
+      Texture.Unlock;
+      Update;
    end Disp_Fill;
 
    --------------
@@ -249,11 +316,14 @@ package body SDL_Display is
    ------------------
 
    function To_SDL_Color (R, G, B : Unsigned_8) return SDL_Pixel is
+      RB : constant Unsigned_16 :=
+        Shift_Right (Unsigned_16 (R), 3) and 16#1F#;
+      GB : constant Unsigned_16 :=
+        Shift_Right (Unsigned_16 (G), 2) and 16#3F#;
+      BB : constant Unsigned_16 :=
+        Shift_Right (Unsigned_16 (B), 3) and 16#1F#;
    begin
-      return SDL_Pixel (SDL_MapRGB (Display.format,
-                        unsigned_char (R),
-                        unsigned_char (G),
-                        unsigned_char (B)));
+      return (Shift_Left (RB, 11) or Shift_Left (GB, 5) or BB);
    end To_SDL_Color;
 
 end SDL_Display;
--- src/sdl_hal_800_480.ads
+++
@@ -1,4 +0,0 @@
-with SDL_Display;
-
-package SDL_HAL_800_480 is new SDL_Display (Width       => 800,
-                                            Height      => 480);
